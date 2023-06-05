59aa29a85258bd9138b0f03d6f0daffa11faab7f
Skyfe24 <fabio.erase@gmail.com>
2023-06-05 19:36:08 +0200

    added While but <li> are not spawning in DOC

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 console.log("JS OK");
 
-const listElement = document.getElementById('lista');
+const result = document.getElementById('lista');
 
-const prodotti = ['pane','latte','insalata','pollo'];
+const listElements = ['pane','latte','insalata','pollo'];
 
-let printItem = '';
-
-for (i=0; i < prodotti.lenght; i++)
-printItem += '<li>prodotti[i]</li>';
-
-listElement.innerHTML = printItem;
+let printList= ' ';
+//
+//for (i=0; i < listElements.lenght; i++) {
+//
+//  printList += `<li>${listElements[i]}</li>`;
+ // 
+//  
+//
+//  
+//}
+//result.innerHTML = printList;
+while (printList <listElements.lenght){
+  printList += `<li>${listElements[i]}</li>`;
+  i++;
+}
+result.innerHTML = printList;
